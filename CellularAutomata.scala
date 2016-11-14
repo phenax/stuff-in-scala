7119bf65a06f352aca3c787e149ec3823db66967
Akshay Nair <phenax5@gmail.com>
2016-11-14 17:09:53 +0530

    Game of life complete(random condition commented out)

--- CellularAutomata.scala
+++ CellularAutomata.scala
@@ -1,6 +1,80 @@
-#!/usr/bin/env scala
+
+import scala.util.Random
 
 
+object CellularAutomata extends App {
+
+	// Start
+	CARunner.init(20, 20)(
+
+		// Anonymous function for rules
+		(prevCell: Boolean, curCell: Boolean, nextCell: Boolean) => 
+			if(curCell == nextCell) true else false
+	);
+}
+
+
+object CARunner {
+
+	// Ignore
+	type B= Boolean;
+
+	def init(row: Int, col: Int)(callback: (B, B, B) => B) {
+
+		var thisRow: Generation= null;
+		var prevRow: Generation= null;
+
+		// Generate first random generation
+		prevRow= new Generation(col);
+		prevRow.fillRandomGeneration();
+
+		// Print the generation
+		prevRow.printGeneration();
+
+
+		for(i <- 1 until row) {
+
+			// Create new generation
+			thisRow= new Generation(col);
+
+			for(j <- 0 until col) {
+
+				// Generate cell based on prevRow
+				val cellState= callback(
+					prevRow.get(j - 1), 
+					prevRow.get(j), 
+					prevRow.get(j + 1)
+				);
+
+				// Give it a state of alive or dead depending 
+				// on what the callback returns
+				thisRow.add(
+					if(cellState)
+						thisRow.ALIVE
+					else
+						thisRow.DEAD
+				);
+
+				// Print the cell
+				thisRow.printCell(j);
+			}
+
+			println();
+
+			// On to the next one
+			prevRow= thisRow;
+		}
+	}
+}
+
+
+
+
+/**
+ * Each generation of cells
+ * 
+ * @param col: Int  Number of cells in a generation
+ */
 class Generation(col: Int) {
 
 	val ALIVE: Boolean= true;
@@ -58,7 +132,7 @@ class Generation(col: Int) {
 
 		for(cell <- this.column) {
 			
-			val random= scala.util.Random;
+			val random= Random;
 			
 			this.add(if(random.nextInt(2) == 1) true else false);
 		}
@@ -66,60 +140,3 @@ class Generation(col: Int) {
 }
 
 
-object CellularAutomata {
-
-	type B= Boolean;
-
-	def init(row: Int, col: Int)(callback: (B, B, B) => B) {
-
-		var thisRow: Generation= null;
-		var prevRow: Generation= null;
-
-		// Generate first random generation
-		prevRow= new Generation(col);
-		prevRow.fillRandomGeneration();
-
-		// Print the generation
-		prevRow.printGeneration();
-
-
-		for(i <- 1 until row) {
-
-			// Create new generation
-			thisRow= new Generation(col);
-
-			for(j <- 0 until col) {
-
-				// Generate cell based on prevRow
-				val cellState= callback(
-					prevRow.get(j - 1), 
-					prevRow.get(j), 
-					prevRow.get(j + 1)
-				);
-
-				// Give it a state of alive or dead depending 
-				// on what the callback returns
-				thisRow.add(
-					if(cellState)
-						thisRow.ALIVE
-					else
-						thisRow.DEAD
-				);
-
-				// Print the cell
-				thisRow.printCell(j);
-			}
-
-			println();
-
-			// On to the next one
-			prevRow= thisRow;
-		}
-	}
-}
-
-// Start
-CellularAutomata.init(20, 20)(
-	(prevCell: Boolean, curCell: Boolean, nextCell: Boolean) => 
-		if(curCell == nextCell) true else false
-);
--- GameOfLife.scala
+++ GameOfLife.scala
@@ -1,116 +1,50 @@
-#!/usr/bin/env scala
 
 import scala.concurrent.{Future}
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.util.Random
 
-class GameOfLifeGeneration(rows: Int, cols: Int) {
 
-	val ALIVE_STATE = '#';
-	val DEAD_STATE  = '-';
 
-	val genGrid= Array.fill[Char](rows, cols) { DEAD_STATE };
+object GameOfLife extends App {
 
-	def renderGrid() {
+	// The rule for the game
+	val golRules= 
+		(live: Int, gen: GameOfLifeGeneration, row: Int, col: Int) => {
 
-		// Loop through the row
-		for(row <- this.genGrid) {
-
-			// Loop through the columns
-			for(col <- row) {
-
-				// Render the cell state
-				print(col + " ");
+			if(gen.genGrid(row)(col) == gen.ALIVE_STATE && live == 3) {
+				true;
+			} else if(live == 2 || live == 3) {
+				true;
+			} else {
+				false;
 			}
+		};
 
-			println();
-		}
-	}
 
-	def setFirstGeneration() {
+	val game= new GameOfLifeRunner(10);
 
-		// val random= new Random();
-
-		this.genGrid(rows/2)(cols/2)= ALIVE_STATE;
-		this.genGrid(rows/2 - 1)(cols/2 - 1)= ALIVE_STATE;
-		this.genGrid(rows/2 - 1)(cols/2)= ALIVE_STATE;
-		this.genGrid(rows/2 - 1)(cols/2 + 1)= ALIVE_STATE;
-	}
-
-	def getNeighbourLiveCount(indexI: Int, indexJ: Int): Int = {
-
-		var numOfLiveCells= 0;
-
-		var m= 0;
-		var n= 0;
-
-		for(i <- -1 to 1) {
-			
-			m= indexI + i;
-
-			for(j <- -1 to 1) {
-
-				n= indexJ + j;
-
-				if(i != 0 || j != 0) {
-					
-					if(m >= rows) {
-						m %= rows;
-					} else if(m < 0) {
-						m = rows + m;
-					}
-
-					if(n >= cols) {
-						n %= cols;
-					} else if(n < 0) {
-						n = cols + n;
-					}
-
-					if(this.genGrid(m)(n) == ALIVE_STATE) {
-						numOfLiveCells += 1;
-					}
-				}
-			}
-		}
-
-		return numOfLiveCells;
-	}
-
-	def inheritFrom(prevGen: GameOfLifeGeneration) {
-
-		for(i <- 0 until rows) {
-
-			for(j <- 0 until cols) {
-
-				var numOfLiveCells= prevGen.getNeighbourLiveCount(i, j);
-
-				if(prevGen.genGrid(i)(j) == ALIVE_STATE) {
-
-					if(numOfLiveCells == 3) {
-						this.genGrid(i)(j)= ALIVE_STATE;
-					}
-
-				} else {
-
-					if(numOfLiveCells == 2 || numOfLiveCells == 3) {
-						this.genGrid(i)(j)= ALIVE_STATE;
-					}
-				}
-			}
-		}
-	}
+	// Set the rules callback
+	game.rules_=(golRules);
+	game.start(50, 80);
 }
 
 
-class GameOfLife(framerate: Int) {
+/**
+ * Game of life generator
+ * 
+ * @param framerate: Int  The number of frames to render in one second
+ */
+class GameOfLifeRunner(framerate: Int) {
 
-	val CLEAR_SCREEN_CODE= "\u001b[H\u001b[2J";
+	private val CLEAR_SCREEN_CODE= "\u001b[H\u001b[2J";
 
 
-	var thisGen: GameOfLifeGeneration= _;
-	var prevGen: GameOfLifeGeneration= _;
+	// The generations
+	private var _thisGen: GameOfLifeGeneration= _;
+	private var _prevGen: GameOfLifeGeneration= _;
 
-	var frameReadyToRender: Boolean= true;
+	// Rules
+	private var _rules: (Int, GameOfLifeGeneration, Int, Int) => Boolean = _;
 
 
 	// Put the current thread to sleep for `time`ms
@@ -119,6 +53,7 @@ class GameOfLife(framerate: Int) {
 	// Clears the terminal screen
 	def clearScreen() = println(CLEAR_SCREEN_CODE);
 
+	// Execute after frame delay
 	def requestNextFrame(callback: ()=> Unit) {
 
 		// 16 second sleep for 60fps
@@ -127,12 +62,16 @@ class GameOfLife(framerate: Int) {
 		callback();
 	}
 
+	def rules_=(callback: (Int, GameOfLifeGeneration, Int, Int) => Boolean) {
+		this._rules= callback;
+	}
+
 	def start(row: Int, col: Int) {
 
-		prevGen= new GameOfLifeGeneration(row, col);
-		prevGen.setFirstGeneration();
+		_prevGen= new GameOfLifeGeneration(row, col);
+		_prevGen.setFirstGeneration();
 
-		// Asynchronous render loop
+		// Asynchronous calculation loop
 		val f = Future {
 			requestNextFrame(
 				this.calculationLoop(row, col)
@@ -153,7 +92,7 @@ class GameOfLife(framerate: Int) {
 		this.clearScreen();
 
 		// Render the stuff to stdout
-		thisGen.renderGrid();
+		_thisGen.renderGrid();
 
 		requestNextFrame(
 			this.runRenderLoop()
@@ -162,11 +101,11 @@ class GameOfLife(framerate: Int) {
 
 	def calculationLoop(row: Int, col: Int)() {
 
-		thisGen= new GameOfLifeGeneration(row, col);
+		_thisGen= new GameOfLifeGeneration(row, col);
 
-		thisGen.inheritFrom(prevGen);
+		_thisGen.inheritFrom(_prevGen)(this._rules);
 
-		prevGen= thisGen;
+		_prevGen= _thisGen;
 
 		requestNextFrame(
 			this.calculationLoop(row, col)
@@ -175,6 +114,118 @@ class GameOfLife(framerate: Int) {
 }
 
 
-val game= new GameOfLife(3);
 
-game.start(50, 50);
+
+/**
+ * Each generation of the game i.e. each frame
+ * 
+ * @param rows: Int  Number of rows in the generation grid
+ * @param cols: Int  Number of cols in the generation grid
+ */
+class GameOfLifeGeneration(rows: Int, cols: Int) {
+
+	// The two cell states
+	val ALIVE_STATE = '#';
+	val DEAD_STATE  = '-';
+
+	// The generation grid
+	val genGrid= Array.fill[Char](rows, cols) { DEAD_STATE };
+
+	// Render the grid to the terminal
+	def renderGrid() {
+
+		// Loop through the row
+		for(row <- this.genGrid) {
+
+			// Loop through the columns
+			for(col <- row) {
+
+				// Render the cell state
+				print(col + " ");
+			}
+
+			println();
+		}
+	}
+
+	// Set the first generation of cells
+	def setFirstGeneration() {
+
+		val random= new Random();
+
+		this.genGrid(rows/2)(cols/2)= ALIVE_STATE;
+		this.genGrid(rows/2 - 1)(cols/2 - 1)= ALIVE_STATE;
+		this.genGrid(rows/2 - 1)(cols/2)= ALIVE_STATE;
+		this.genGrid(rows/2 - 1)(cols/2 + 1)= ALIVE_STATE;
+
+		// for(i <- 0 until rows) {
+
+		// 	for(j <- 0 until cols) {
+
+		// 		this.genGrid(i)(j)= 
+		// 			if(random.nextInt(10) == 0)
+		// 				this.ALIVE_STATE
+		// 			else
+		// 				this.DEAD_STATE;
+		// 	}
+		// }
+	}
+
+	def getNeighbourLiveCount(indexI: Int, indexJ: Int): Int = {
+
+		var numOfLiveCells= 0;
+
+		var m= 0;
+		var n= 0;
+
+		for(i <- -1 to 1) {
+			
+			m= indexI + i;
+
+			for(j <- -1 to 1) {
+
+				n= indexJ + j;
+
+				if(i != 0 || j != 0) {
+					
+					if(m >= rows)
+						m %= rows;
+					 else if(m < 0)
+						m = rows + m;
+
+					if(n >= cols)
+						n %= cols;
+					 else if(n < 0)
+						n = cols + n;
+
+					if(this.genGrid(m)(n) == ALIVE_STATE)
+						numOfLiveCells += 1;
+				}
+			}
+		}
+
+		return numOfLiveCells;
+	}
+
+	// Inherit cell properties from the previous generation with the help of rules
+	def inheritFrom(prevGen: GameOfLifeGeneration)(rules: (Int, GameOfLifeGeneration, Int, Int) => Boolean) {
+
+		for(i <- 0 until rows) {
+
+			for(j <- 0 until cols) {
+
+				val numOfLiveCells= prevGen.getNeighbourLiveCount(i, j);
+
+				val nextCellState= rules(numOfLiveCells, prevGen, i, j);
+
+				this.genGrid(i)(j)= 
+					if(nextCellState)
+						this.ALIVE_STATE
+					else
+						this.DEAD_STATE;
+			}
+		}
+	}
+}
+
+
